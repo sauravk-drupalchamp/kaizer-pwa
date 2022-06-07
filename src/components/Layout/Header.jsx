78ae606c81d957e612ed2be7200c2d4c7905d4a3
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-06-07 16:23:17 +0530

    Adding Changes

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import {React, Fragment} from 'react';
+import {Row, Col} from 'antd';
+import './Header.module.css';
+import logo from '../../assets/logo.png';
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Fragment>
+    <Row className='header-wrapper'>
+      <Col span={4}>
+      <a href="/"><img src={logo} alt="logo" /></a>
+      </Col>
+      <Col span={20}>
+        Right
+      </Col>
+    </Row>
+    
+    </Fragment>
   )
 }
 
