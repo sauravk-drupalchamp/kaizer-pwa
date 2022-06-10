03e5005de077f7619d193ca0bb153d9b624901f3
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-06-10 17:21:06 +0530

    Adding Changes

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,10 +1,15 @@
 import { React, Fragment } from "react";
 import { Row, Col, Menu } from "antd";
-import { AppstoreOutlined, HomeOutlined } from '@ant-design/icons'
+import { Avatar } from 'antd';
+import { AppstoreOutlined, UserOutlined } from '@ant-design/icons'
 import "./Header.css";
 import logo from "../../assets/logo.png";
 
 const Header = () => {
+
+  const handleClick = () =>{
+    alert(window.location.pathname);
+  }
 
   return (
     <Fragment>
@@ -14,15 +19,17 @@ const Header = () => {
             <img src={logo} alt="logo" />
           </a>
         </Col>
-        <Col span={20}>
+        <Col span={16}>
           <Menu mode="horizontal" defaultSelectedKeys={["home"]}>
-            <Menu.Item key="home" icon={<HomeOutlined />}><a href="/">Home</a></Menu.Item>
+            <Menu.Item key="constructionSites"><a href="/construction-sites">Construction Sites</a></Menu.Item>
             <Menu.SubMenu key="Menu" title="Menu" icon={<AppstoreOutlined />}>
               <Menu.Item key="addToolbox"><a href="/add-toolbox">Add Toolbox</a></Menu.Item>
-              <Menu.Item key="constructionSites"><a href="/construction-sites">Construction Sites</a></Menu.Item>
               <Menu.Item key="externalWorker"><a href="/add-external-worker">External Worker</a></Menu.Item>
             </Menu.SubMenu>
           </Menu>
+        </Col>
+        <Col span={4}>
+        <Avatar onClick={handleClick} size="large" icon={<UserOutlined />} />
         </Col>
       </Row>
     </Fragment>
