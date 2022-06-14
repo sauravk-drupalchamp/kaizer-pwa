b04e2880089b78988afe83d810c9f9a299dae0d9
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-06-14 16:14:08 +0530

    Adding Changes

--- src/components/Pages/ConstructionSites.jsx
+++ src/components/Pages/ConstructionSites.jsx
@@ -1,22 +1,71 @@
-import { React, useEffect } from 'react'
-import Config from '../../config'
-import axios from 'axios'
+import { React, useEffect } from "react";
+import Config from "../../config";
+import axios from "axios";
 
 const ConstructionSites = () => {
+  useEffect(() => {
+    var username = "admin";
+    var password = "admin";
+    var basicAuth = "Basic " + btoa(username + ":" + password);
 
-  useEffect(()=>{
-    axios.get(`${Config.drupal_local_url}/rest/article`).then((res)=>{
-    console.warn(res.data.map((item,i)=>{
-      return item.title;
-    }))
-  }).catch((err)=>{
-    console.warn(err)
-  })
-  },[])
+    axios
+      .get(`${Config.drupal_local_url}/session/token`)
+      .then((res) => {
+        console.warn(res.data);
 
-  return (
-    <h1>ConstructionSites</h1>
-  )
-}
+        const headers = {
+          "Content-Type": "application/hal+json",
+          "X-CSRF-Token": res.data,
+          "Access-Control-Expose-Headers": "*",
+          "Authorization": basicAuth,
+        };
 
-export default ConstructionSites
+        axios
+          .post(
+            `${Config.drupal_local_url}/node/10`,
+            {
+              _links: {
+                type: {
+                  href: "http://drupal-121194-391381.cloudwaysapps.com/rest/type/node/page",
+                },
+              },
+              title: [
+                {
+                  value: "My first page",
+                },
+              ],
+            },
+            {
+              headers,
+            }
+          )
+          .then((response) => (console.log(response)));
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+    // var node = {
+    //   type: [{
+    //     target_id: 'article',
+    //     target_type: 'node_type',
+    //   }],
+    //   title: [{
+    //     value: 'Dummy',
+    //   }],
+    //   body: [{
+    //     value: 'Dummy Body',
+    //     format: 'plain_text',
+    //   }],
+    // };
+
+    // axios.post(`${Config.drupal_local_url}/node`, node).then((res)=>{
+    //   console.log(res,'=====RESPONSE')
+    // }).catch((err)=>{
+    //   console.log(err)
+    // })
+  }, []);
+
+  return <h1>ConstructionSites</h1>;
+};
+
+export default ConstructionSites;
