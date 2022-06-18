eb70fd32ed6ee47ec1d3354d0d474b85bef86e47
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-06-19 00:40:58 +0530

    Adding changes

--- src/components/Pages/ConstructionSitesDetails.jsx
+++ src/components/Pages/ConstructionSitesDetails.jsx
@@ -1,42 +1,40 @@
 import {React, useState, useEffect} from 'react'
+import { useParams } from "react-router-dom";
 import config from '../../config';
 import axios from 'axios';
 
 const ConstructionSitesDetails = () => {
   const [constructionSiteData, setConstructionSiteData] = useState([]);
   const [ isLoaded, setIsLoaded ] = useState(false);
-  //const [nid, setNid] = useState();
-
-  const siteID = (window.location.pathname).substr(27);
-  // console.log(siteID);
+  // const [nid, setNid] = useState();
+  const siteID   = useParams(); 
 
   useEffect(()=>{
-    axios.get(`${config.drupal_live_url}/construction-sites-detail-rest-api/${siteID}`).then((constructionSiteResponse)=>{
+    axios.get(`${config.drupal_live_url}/construction-sites-detail-rest-api/${siteID.id}`).then((constructionSiteResponse)=>{
       setConstructionSiteData(constructionSiteResponse.data);
       // console.log(constructionSiteResponse);
-      //setNid(constructionSiteResponse.data[0].nid,"constructionSiteResponse.data[0].nid");
-      // console.log(constructionSiteResponse.data[0].nid,"NID");
+      // setNid(constructionSiteResponse.data[0].nid);
+      // console.log(nid);
 
-      // axios.get(`${config.drupal_live_url}/toolboxes-listing-rest-api/${nid}`).then((toolboxesResponse)=>{
-      //   console.log(toolboxesResponse.data)
-      // }).catch((toolboxesError)=>{
-      //   console.log("toolboxesError",toolboxesError)
-      // })
-      //console.log(`${config.drupal_live_url}/toolboxes-listing-rest-api/${nid}`)
+        // axios.get(`${config.drupal_live_url}/toolboxes-listing-rest-api/75`).then((toolboxesResponse)=>{
+        //   console.log(toolboxesResponse.data)
+        // }).catch((toolboxesError)=>{
+        //   console.log("toolboxesError",toolboxesError)
+        // })
+      // console.log(`${config.drupal_live_url}/toolboxes-listing-rest-api/${nid}`)
       setIsLoaded(true)
     }).catch((constructionSiteError)=>{
       console.log("constructionSiteError",constructionSiteError)
     })
-  });
+  },[]);
+
   if(!isLoaded){
     return <h1>Loading .......</h1>
   }else{
       return(
         constructionSiteData.map((item,index)=>{
-          // if(nid){
-          //   setNid(item.nid)
-          // }
-          //console.log(nid,"NID")
+          // setNid(item.nid)
+          // console.log(nid,"NID")
           return <div>
             <p>General Info</p>
             <p>{item.title}</p>
