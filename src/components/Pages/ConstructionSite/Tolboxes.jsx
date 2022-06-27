54628be8572f67b5d1549a681759aa83bd19806d
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-06-27 16:28:28 +0530

    Adding Changes for Delete Toolboxes

--- src/components/Pages/ConstructionSite/Tolboxes.jsx
+++ src/components/Pages/ConstructionSite/Tolboxes.jsx
@@ -1,49 +1,102 @@
 import { React, useState, useEffect } from "react";
-import { Button, Table, Spin } from 'antd'
+import { Button, Table, Spin, message } from "antd";
 import { EyeOutlined, DeleteTwoTone } from "@ant-design/icons";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Config from "../../../config";
 import axios from "axios";
 
 const Tolboxes = (props) => {
-    const [toolboxesInfo, setToolboxesInfo] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const toolboxesInfoUrl = `${Config.drupal_live_url}/toolboxes-listing-rest-api/${props.siteID}`;
-    const { tableData, columns } = getTableData();
-    useEffect(() => {
-        axios
-          .get(toolboxesInfoUrl)
-          .then((toolboxesInfoResponse) => {
-            setToolboxesInfo(toolboxesInfoResponse.data);
-            // console.log(toolboxesInfoResponse.data);
-            setIsLoaded(true);
-          })
-          .catch((toolboxesInfoError) => {
-            console.log(toolboxesInfoError, "toolboxesInfoError");
-          });
-      }, [toolboxesInfoUrl]);
+  const [toolboxesInfo, setToolboxesInfo] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const toolboxesInfoUrl = `${Config.drupal_live_url}/toolboxes-listing-rest-api/${props.siteID}`;
+  const { tableData, columns } = getTableData();
+  const nav = useNavigate();
+  const siteID = useParams();
+  useEffect(() => {
+    axios
+      .get(toolboxesInfoUrl)
+      .then((toolboxesInfoResponse) => {
+        setToolboxesInfo(toolboxesInfoResponse.data);
+        // console.log(toolboxesInfoResponse.data);
+        setIsLoaded(true);
+      })
+      .catch((toolboxesInfoError) => {
+        console.log(toolboxesInfoError, "toolboxesInfoError");
+      });
+  }, [toolboxesInfoUrl]);
 
-    return !isLoaded ? (
-    <h1><Spin size="large" /></h1>
+  return !isLoaded ? (
+    <h1>
+      <Spin size="large" />
+    </h1>
   ) : (
     <div className="toolboxes-info-wrapper">
-    <div className="toolboxes-info-heading">
-    <h4>Toolboxes Info</h4>
-    <Link to={`/add-toolbox/${props.siteID}`}><Button type="primary">+</Button></Link></div><hr />
+      <div className="toolboxes-info-heading">
+        <h4>Toolboxes Info</h4>
+        <Link to={`/add-toolbox/${props.siteID}`}>
+          <Button type="primary">+</Button>
+        </Link>
+      </div>
+      <hr />
       <Table dataSource={tableData} columns={columns} />
     </div>
   );
-  
+
   function getTableData() {
     const tableData = toolboxesInfo.map((item, index) => {
       const title = item.title;
       const date_from = item.field_date_from;
 
+      const handleDeleteClick = (e) => {
+        // console.log(e.currentTarget.id)
+        const nid = e.currentTarget.id;
+
+        axios
+          .get(`${Config.drupal_live_url}/session/token`)
+          .then((tokenResponse) => {
+            axios({
+              method: "delete",
+              url: `${Config.drupal_live_url}/node/${nid}?_format=json`,
+              headers: {
+                "X-CSRF-Token": `${tokenResponse.data}`,
+              },
+              auth: {
+                username: `${sessionStorage.getItem("username")}`,
+                password: `${sessionStorage.getItem("password")}`,
+              },
+            })
+              .then((deleteResponse) => {
+                // console.log("deleteResponse",deleteResponse)
+                if(deleteResponse.status === 204){
+                  message.success("Deleted Successfully")
+                  // nav(`/construction-sites-detail/${siteID.id}`);
+                  window.location.href = `/construction-sites-detail/${siteID.id}`;
+                }else{
+                  message.error("Ooops something went wrong!")
+                }
+              })
+              .catch((deleteError) => {
+                console.log("deleteError",deleteError)
+              });
+          })
+          .catch((tokenError) => {
+            console.log("tokenError", tokenError);
+          });
+      };
+
       return {
         key: index,
         name: title,
         date_from: date_from,
-        action_view: <div><EyeOutlined /><DeleteTwoTone /> </div>,
+        action_view: (
+          <div>
+            <EyeOutlined />{" "}
+            <span onClick={handleDeleteClick} id={item.nid}>
+              {" "}
+              <DeleteTwoTone />
+            </span>{" "}
+          </div>
+        ),
       };
     });
 
@@ -66,7 +119,6 @@ const Tolboxes = (props) => {
     ];
     return { tableData, columns };
   }
+};
 
-}
-
-export default Tolboxes
+export default Tolboxes;
