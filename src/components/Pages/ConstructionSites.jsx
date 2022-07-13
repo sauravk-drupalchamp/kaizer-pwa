dbf0cfb8e75d1c6db30673b4c7c15e6e51dd1056
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-13 15:44:00 +0530

    Added Offline Support for Construction Sites page

--- src/components/Pages/ConstructionSites.jsx
+++ src/components/Pages/ConstructionSites.jsx
@@ -9,6 +9,7 @@ import axios from "axios";
 const ConstructionSites = () => {
   const [items, setItems] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [networkDataReceived, setNetworkDataReceived] = useState(true)
 
   const { tableData, columns } = getTableData();
 
@@ -21,41 +22,80 @@ const ConstructionSites = () => {
       })
       .catch((err) => {
         console.warn(err);
+        // if(!window.navigator.onLine){
+        //   setIsLoaded(true)
+        // }
+        setNetworkDataReceived(false);
       });
-  }, []);
+    // fetch(`${Config.drupal_live_url}/construction-sites`)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setItems(data);
+    //     setIsLoaded(true);
+    //   })
+    //   .catch((err) => {
+    //     console.log("Fetch Err",err)
+    //     setNetworkDataReceived(false)
+    //   });
+
+      if(!window.navigator.onLine){
+        if ('caches' in window) {
+          console.log("Caches in Windows")
+          caches.match(`${Config.drupal_live_url}/construction-sites`)
+            .then(function(response) {
+              if (response) {
+                return response.json();
+              }
+            })
+            .then(function(data) {
+              if (!networkDataReceived) {
+                console.log('From cache', data);
+                setItems(data);
+                setIsLoaded(true);
+              }
+            });
+        }
+      }
+  }, [networkDataReceived]);
 
   return !isLoaded ? (
     <Spin size="large" />
   ) : (
     <Fragment>
       <Row className="construction-sites-row">
-          <Col xs={{
-        span: 22,
-        offset: 1,
-      }}
-      lg={{
-        span: 16,
-        offset: 4,
-      }}
-      md={{
-        span: 20,
-        offset: 2,
-      }}
-      >
-            <h1>Construction Sites</h1>
-          </Col>
-        <Col xs={{
-        span: 22,
-        offset: 1,
-      }}
-      lg={{
-        span: 16,
-        offset: 4,
-      }}
-      md={{
-        span: 20,
-        offset: 2,
-      }} className="construction-sites-col">
+        <Col
+          xs={{
+            span: 22,
+            offset: 1,
+          }}
+          lg={{
+            span: 16,
+            offset: 4,
+          }}
+          md={{
+            span: 20,
+            offset: 2,
+          }}
+        >
+          <h1>Construction Sites</h1>
+        </Col>
+        <Col
+          xs={{
+            span: 22,
+            offset: 1,
+          }}
+          lg={{
+            span: 16,
+            offset: 4,
+          }}
+          md={{
+            span: 20,
+            offset: 2,
+          }}
+          className="construction-sites-col"
+        >
           <h5>Construction Sites</h5>
           <Table
             className="construction-sites-table"
@@ -80,7 +120,7 @@ const ConstructionSites = () => {
         tool_sign: (
           <Progress
             percent={per}
-            status={per <=  50 ? "exception" : null}
+            status={per <= 50 ? "exception" : null}
             size="small"
           />
         ),
