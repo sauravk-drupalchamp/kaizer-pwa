2fb697365556fd9263013b169036cb7f03e03cd9
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-26 12:23:20 +0530

    Added Changes for Homepage and Back button

--- src/components/Layout/NoMatch.jsx
+++ src/components/Layout/NoMatch.jsx
@@ -1,17 +1,18 @@
 import { React, Fragment } from "react";
-import { Row, Col } from "antd";
-import { useLocation, Link } from "react-router-dom";
+import { Row, Col, Button } from "antd";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 
 const NoMatch = () => {
   const location = useLocation();
-
+  const navigate = useNavigate();
   return (
     <Fragment>
       <Row>
         <Col offset={4} span={16} className='no-match-page'>
           <h1>404 Page Not Found..</h1>
           <h2>Wrong URL {location.pathname}</h2>
-          <Link to='/'>Homepage</Link>
+          <Link to='/'><Button type="primary">Homepage</Button></Link>
+          <Button type="secondary" onClick={() => navigate(-1)}>Go Back</Button>
         </Col>
       </Row>
     </Fragment>
